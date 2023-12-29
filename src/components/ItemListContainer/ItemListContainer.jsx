a67358662efc723cbeae5ee8c76be72ff21ec0ee
Charlie Barié <69181864+charbarie@users.noreply.github.com>
2023-12-29 12:46:50 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,41 +2,39 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CardList from '../CardList/CardList.jsx';
 import LoadingIndicator from '../LoadingIndicator/LoadingIndicator';
-import { getDocs, collection, query, where } from 'firebase/firestore';
+import { getDocs, collection, query, where, getFirestore, doc } from 'firebase/firestore';
 import { firebaseConnection } from '/src/services/firebase/firebaseConfig.js';
 
 const ItemListContainer = () => {
   const [products, setProducts] = useState([]);
+
   const [loading, setLoading] = useState(true);
   const { categoryId } = useParams();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
 
-        const collectionRef = categoryId
-          ? query(collection(firebaseConnection, 'products'), where('category', '==', categoryId))
-          : collection(firebaseConnection, 'products');
+    const dbFirebase = getFirestore()
+    const queryCollection = collection(dbFirebase, 'Productos')
+    getDocs(queryCollection)
+      .then(res => setProducts(res.docs.map(producto => ({ id: producto.id, ...producto.data() }))))
+      .catch(err => console.log(err))
+      .finally(() => setLoading(false))
 
-        const snapshot = await getDocs(collectionRef);
-        const productsAdapted = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-        setProducts(productsAdapted);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
 
-    fetchData();
-  }, [categoryId]);
+  }, [])
+
+  console.log(products)
+
+
+
 
   return (
     <>
+
       {loading ? <LoadingIndicator /> : <CardList products={products} />}
     </>
   );
 };
 
 export default ItemListContainer;
+
