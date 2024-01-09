524e3649a120ffde65990ea1e940896904e5ac30
Charlie Barié <char.barie@gmail.com>
2024-01-09 14:14:55 -0300

    fix

--- src/components/ItemCounter/ItemCounter.jsx
+++ src/components/ItemCounter/ItemCounter.jsx
@@ -1,16 +1,21 @@
-/* eslint-disable react/prop-types */
-
+import { useState } from 'react';
 import { useCounter } from '../hooks/useCounter';
 import Button from 'react-bootstrap/Button';
 import './ItemCounter.css';
 import { Link } from 'react-router-dom';
-
+import PropTypes from 'prop-types';
 export const ItemCounter = ({ initial = 1, stock, onAddToCart, onFinish }) => {
   const { counter, handleSum, handleRest } = useCounter(initial, stock);
+  const [addedToCart, setAddedToCart] = useState(false);
 
   const handleFinish = () => {
     console.log('Producto finalizado. Cantidad:', counter);
     onFinish(counter);
+  };
+
+  const handleAddToCart = () => {
+    onAddToCart(counter);
+    setAddedToCart(true);
   };
 
   return (
@@ -21,14 +26,20 @@ export const ItemCounter = ({ initial = 1, stock, onAddToCart, onFinish }) => {
         <Button size="sm" onClick={handleSum}> + </Button>
       </div>
       <div className='ButtonAdd'>
-
-        <Button variant="outline-success" onClick={() => onAddToCart(counter)} disabled={counter === 0}> Agregar </Button>
-        <Link to="/Checkout">
-          <Button variant="outline-primary" onClick={handleFinish} disabled={counter === 0}> Finalizar </Button>
-        </Link>
+        <Button variant="outline-success" onClick={handleAddToCart} disabled={counter === 0 || addedToCart}> Agregar </Button>
+        {addedToCart && (
+          <Link to="/Checkout">
+            <Button variant="outline-primary" onClick={handleFinish} disabled={counter === 0}> Finalizar </Button>
+          </Link>
+        )}
       </div>
     </>
   );
+}; ItemCounter.propTypes = {
+  initial: PropTypes.number,
+  stock: PropTypes.number.isRequired,
+  onAddToCart: PropTypes.func.isRequired,
+  onFinish: PropTypes.func.isRequired,
 };
 
 export default ItemCounter;
